ff43e850815f8d7dceb40b1e24e907b91c7f335a
brj01 <167606460+brj01@users.noreply.github.com>
2025-10-26 13:42:33 +0300

    hi

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+// Set custom favicon and page title at runtime using the image in src/assets
+import favicon from "./assets/images/download (19).jpeg";
+
+// Set the browser tab title
+document.title = "Barea Joudi portfolio";
+
+// Update or create favicon link in the document head
+function setFavicon(href) {
+	let link = document.querySelector("link[rel~='icon']");
+	if (!link) {
+		link = document.createElement('link');
+		link.rel = 'icon';
+		document.getElementsByTagName('head')[0].appendChild(link);
+	}
+	link.href = href;
+}
+
+setFavicon(favicon);
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
