c7df17e0cb8a715ec47847d204f02ba5a6df3101
Hussein Atoui <husseinatoui20@gmail.com>
2025-10-27 12:06:37 +0200

    contact section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
+import Contact from "./containers/contact/Contact"; // import the footer
 
 function App() {
   return (
-    <div>
+    <div className="app-container">
       <Main />
+
     </div>
   );
 }
